f92f4295e6724bea58506035265031b1b908a342
saiprasad88 <tsaiprasad88@gmail.com>
2020-04-14 16:13:31 +0530

    test commit

---
+++ test11.py
@@ -0,0 +1,13 @@
+phrase = "emmanuel  macron"
+print(phrase.upper())
+print(phrase.lower())
+print(phrase  +  " is the president")
+print(phrase.isupper())
+print(phrase.islower())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("nue"))
+print(phrase.index("n"))
+print(phrase.index("on"))
+
+
