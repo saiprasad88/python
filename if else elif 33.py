88fe58db0a2bc675506ae9ea0892b5ea75517070
saiprasad88 <tsaiprasad88@gmail.com>
2020-04-15 16:59:40 +0530

    test commit

--- if else elif 33.py
+++ if else elif 33.py
@@ -1,6 +1,6 @@
 is_male = True
 is_tall = True
-if is_male and is_tall:
+if is_male o is_tall:
     print("you are male, you are tall")
 else:
     print("you  are neither male nor tall or both ")
--- if et else 32.py
+++ if et else 32.py
@@ -1,6 +1,11 @@
-is_male = True
-is_tall = False
+is_male = False
+is_tall = True
 if is_male and is_tall:
-    print("you are male, you are tall")
+    print("you are a tall male")
+elif is_male and not(is_tall):
+    print("you are a short male")
+elif not(is_male) and is_tall:
+    print("you are not male, you are tall ")
 else:
+
     print("you  are neither male nor tall or both ")
