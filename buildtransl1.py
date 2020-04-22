6a6386ab80560f8de5aafa8232a67c9ab3de7298
saiprasad88 <tsaiprasad88@gmail.com>
2020-04-22 12:54:02 +0530

    buildbasics

---
+++ buildtransl1.py
@@ -0,0 +1,11 @@
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+
+print(translate(input("enter a phrase")))
